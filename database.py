08a786c8b23a322883bda8c050a3bba4c4f43776
finfan <finex.dev@gmail.com>
2023-01-15 15:58:25 +0300

    Upload

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import logging
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
@@ -6,26 +7,23 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-DB_NAME = 'restraunt'
-USER = 'postgres'
-PSWD = '2236'
-HOST = 'localhost'
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 Base = declarative_base()
-
 
 class Menu(Base):
     __tablename__: str = 'menu'
     id: int = Column(Integer, primary_key=True)
-    name: str = Column(String, unique_key=True)
+    name: str = Column(String, unique=True)
     description: str = Column(String)
     submenus = relationship("SubMenu", backref="menu", cascade="all, delete-orphan")
-
 
 class SubMenu(Base):
     __tablename__: str = 'submenu'
     id: int = Column(Integer, primary_key=True)
-    name: str = Column(String, unique_key=True)
+    name: str = Column(String, unique=True)
     dishes = relationship("Dish", backref="submenu", cascade="all, delete-orphan")
     menu_id: int = Column(Integer, ForeignKey("menu.id"), nullable=False)
 
@@ -33,11 +31,11 @@ class SubMenu(Base):
 class Dish(Base):
     __tablename__: str = 'dish'
     id: int = Column(Integer, primary_key=True)
-    name: str = Column(String, unique_key=True)
+    name: str = Column(String, unique=True)
     price: int = Column(Integer)
     weight: int = Column(Integer)
     submenu_id: int = Column(Integer, ForeignKey("submenu.id"), nullable=False)
 
 
-engine = create_engine("postgresql://postgres:2236@localhost:5432/pytest?user", echo=True, future=True)
+engine = create_engine("postgresql://postgres:2236@localhost:5432/pytest", future=True)
 Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from database import *
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 from sqlalchemy import select
+from sqlalchemy import insert
 
 app = FastAPI()
 
@@ -14,8 +16,9 @@ async def root():
     return {"message": "Hello World"}
 
 
+# Создает меню
 @app.post(DEFAULT_WAY + "api/v1/menus")
-async def createMenu(name: str = 'My menu 1', description: str = 'My menu description 1'):
+async def create_menu(name: str = 'My menu 1', description: str = 'My menu description 1'):
     with Session(engine) as session:
         menu = Menu(
             name=name,
@@ -35,13 +38,37 @@ async def createMenu(name: str = 'My menu 1', description: str = 'My menu descri
         )
         session.add(menu)
         session.commit()
+        return menu
 
 
+# Просматривает список меню
 @app.get(DEFAULT_WAY + "api/v1/menus")
-async def getMenus():
+async def menu_list():
+    all_menus: list = list()
     with Session(engine) as session:
-        stmt = select(Menu).where(Menu.id.in_(["spongebob", "sandy"]))
+        for next_menu in session.query(Menu):
+            all_menus.append(next_menu)
+        return all_menus
 
+
+# Просматривает определенное меню
+@app.get(DEFAULT_WAY + "api/v1/menus/{target_menu_id}")
+async def get_menu(target_menu_id: int):
+    with Session(engine) as session:
+        for next_menu in session.query(Menu).where(Menu.id == target_menu_id):
+            return next_menu
+
+
+# Обновляет меню
+@app.patch(DEFAULT_WAY + "api/v1/menus/{target_menu_id}")
+async def update_menu(target_menu_id: int):
+    update_stmt = (
+        update(Menu)
+        .where(Menu.id == target_menu_id)
+        .values(name="Patrick the Star")
+        .returning(Menu.id, Menu.name, Menu.description)
+    )
+    return update_stmt
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
